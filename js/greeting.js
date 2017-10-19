03d821b591acd8740518011e46aff7e890cb9be5
Daniel Koster <P15206165@LEC-ADMIN.dmu.ac.uk>
2017-10-19 10:38:23 +0100

    added header tag

--- js/greeting.js
+++ js/greeting.js
@@ -2,17 +2,16 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let myHeader = document.getElementById("my-header");
+  let userGreeting = document.getElementById("user-greeting");
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
+	myHeader.innerHTML = userGreeting.value + " " + userName.value;
     event.preventDefault();
     return false;
   }
 
-  if (typeof event === "undefined") {
-    getName.onsubmit = PerformGreeting; //Firefox
-  } else {
     getName.addEventListener("submit", PerformGreeting);
-  }
-  
+   
 }());
